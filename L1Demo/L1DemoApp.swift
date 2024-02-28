bacd89d1d0d1fc77341c266207e403a9db6b000e
erwinagpasa <rootaccess000@gmail.com>
2024-02-28 09:14:52 +0300

    Initial Commit

---
+++ L1Demo/L1DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1DemoApp.swift
+//  L1Demo
+//
+//  Created by Developer on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct L1DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
