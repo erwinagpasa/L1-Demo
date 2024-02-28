aa1c64bcecad202bc74d6b7007574d76451cc415
erwinagpasa <rootaccess000@gmail.com>
2024-02-28 09:50:42 +0300

    Lesson 1

--- L1Demo/ContentView.swift
+++ L1Demo/ContentView.swift
@@ -9,14 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+      
+      ZStack {
+        Color(.black).ignoresSafeArea()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Image("erwin")
+            .resizable()
+            .cornerRadius(20.0)
+            .aspectRatio(contentMode: .fit)
+
+          Text("Erwin Agpasa")
+            .font(.title)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .foregroundColor(Color.white)
         }
-        .padding()
-    }
+      }
+      
+
+     }
 }
 
 #Preview {
